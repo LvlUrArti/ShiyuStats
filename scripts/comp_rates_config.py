3b0f219e476e1dd1bc0936ff607e391f8b1fb9e8
Rhapsodya Piedro Asmorobangun <piedrorhapsodya@gmail.com>
2026-08-30 21:55:22 +0700

    chore: 3.1.3 update

--- scripts/comp_rates_config.py
+++ scripts/comp_rates_config.py
@@ -29,7 +29,7 @@ parser.add_argument("-n", "--no", action="store_true")
 
 args = parser.parse_args()
 
-RECENT_PHASE: str = args.version or "3.1.2"
+RECENT_PHASE: str = args.version or "3.1.3"
 
 
 def relative_path(relative_path: str) -> str:
